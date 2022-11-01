ea1a2909d2e8fa098392d5fb66e9df3662d03839
konstantin019 <113022558+konstantin019@users.noreply.github.com>
2022-11-01 19:23:19 +0500

    Create Class1.cs

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿include < iosteream >
+
+using namespase std;
+
+        int n, summ = 0;
+        cin >> n
+        int a = n, b = n * 2, c = 1;
+        int main(0)
+        {
+            if (a < b){
+                c *= a;
+                a += 1;
+            }
+            else
+            {
+                summ += c;
+                c = 0;
+                a = n - 1;
+                b = a * 2;
+                if (a == 0)
+                {
+                    cout<<summ<<endl
+                }
+            }
+        }
+    }
+}
