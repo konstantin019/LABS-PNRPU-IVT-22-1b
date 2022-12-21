7e086c7080c7b4b8d3c5d0e131b3e68a0bb52f0f
konstantin019 <113022558+konstantin019@users.noreply.github.com>
2022-12-21 17:32:26 +0500

    Add files via upload

---
+++ коды/задание 3,5.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    int n, fact = 1;
+    int* f = &fact;
+
+    std::cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        *f *= i;
+    }
+    std::cout << "Factorial of " << n << " is " << fact << std::endl;
+}
