7fa808910935e4ebf85617cb7b6297f2a4d9285d
konstantin019 <113022558+konstantin019@users.noreply.github.com>
2022-12-21 16:07:55 +0500

    Add files via upload

--- коды/метод касательных.cpp
+++ коды/метод касательных.cpp
@@ -4,7 +4,7 @@ int main() {
     double a = 1, b = 2, c = b, d = a;
     while (std::abs(c - d) > 0.000001) {
         d = c;
-        double k = (std::acos(c) - std::sqrt(1 - 0.3 * pow(c, 3)))/c, e = std::acos(a) - std::sqrt(1 - 0.3 * pow(a, 3)) - k * c;
+        double k = (std::acos(c) - std::sqrt(1 - 0.3 * pow(c, 3))) / c, e = std::acos(c) - std::sqrt(1 - 0.3 * pow(c, 3)) - k * c;
         c = -e / k;
     }
     std::cout << c << std::endl;
