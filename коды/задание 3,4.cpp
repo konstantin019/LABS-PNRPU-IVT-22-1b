fde2cb49b9f539fa24cbabc72bbda2f252919aa6
konstantin019 <113022558+konstantin019@users.noreply.github.com>
2022-12-21 17:30:54 +0500

    Add files via upload

---
+++ коды/задание 3,4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int n, tmp, * ptr = &tmp, max, min;
+    std::cout << "Введите длину последовательности:\n";
+    std::cin >> n;
+    std::cout << "Введите первое число:\n";
+    std::cin >> *ptr;
+    max = *ptr;
+    min = *ptr;
+    for (int i = 2; i <= n; i++)
+    {
+        std::cin >> *ptr;
+        if (*ptr > max) { max = *ptr; }
+        else if (*ptr < min) { min = *ptr; }
+    }
+    std::cout << "Максимальный элемент: " << max << std::endl;
+    std::cout << "Минимальный элемент: " << min << std::endl;
+}
