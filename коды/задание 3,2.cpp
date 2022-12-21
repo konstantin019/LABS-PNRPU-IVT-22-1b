82a247cd9ef1913bfc3e9d2b13b20b056802c6e7
konstantin019 <113022558+konstantin019@users.noreply.github.com>
2022-12-21 17:26:32 +0500

    Add files via upload

---
+++ коды/задание 3,2.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int a, b;
+	std::cout << "Введите первое число" << std::endl;
+	std::cin >> a;
+	std::cout << "Введите второе число" << std::endl;
+	std::cin >> b;
+	int* prt2 = &a;
+	int* prt1 = &b;
+	*prt1 += *prt2;
+	std::cout << *prt1 << std::endl;
+}
