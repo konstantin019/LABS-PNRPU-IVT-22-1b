1e67e9b51a9d83ee1305a9f54953d738ca6db003
konstantin019 <113022558+konstantin019@users.noreply.github.com>
2022-12-21 16:23:59 +0500

    Update метод касательных.cpp

--- коды/решение нелинейных уравнений/метод касательных/метод касательных.cpp
+++ коды/решение нелинейных уравнений/метод касательных/метод касательных.cpp
@@ -1,10 +1,29 @@
-﻿#include <iostream> 
+// f(x) = arccos(x) - sqrt(1 - 0.3 * x^3);
+// f'(x) = -1/(sqrt(1-x^2)) + (0.3 * 3 * x^2)/(2*sqrt(1 - 0.3 * x^3);
+// f''(x) = -x/(sqrt(1-x^2))^3 + ((3.6*x*sqrt(1- 0.3x^2) + (0.81*x^4)/(sqrt(1 - 0.3x^2))))/(2 * sqrt(1-0.3x^3))^2;
+// [0,1];
+// f(b) * f''(b) > 0;
+// -0.83 * 2,38 = -1,9754 < 0 => x0 = a;
+
+#include <iostream>
 #include <cmath>
-int main() {
-    double a = 1, b = 2, c = b, d = a;
-    while (std::abs(c - d) > 0.000001) {
-        d = c;
-        double k = (std::acos(c) - std::sqrt(1 - 0.3 * pow(c, 3))) / c, e = std::acos(c) - std::sqrt(1 - 0.3 * pow(c, 3)) - k * c;
-        c = -e / k;
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    float x, xPrev;
+    x = 0;
+    xPrev = -2;
+    double eps = 0.000001;
+
+    while (abs(x - xPrev) > eps)
+    {
+        xPrev = x;
+        x = xPrev - ((acos(xPrev) - sqrt(1 - 0.3 * pow(xPrev, 3))) / (-1 / (sqrt(1 - pow(xPrev,2) )) + (0.3 * 3 * pow(xPrev,2)) / (2 * sqrt(1 - 0.3 * pow(xPrev,3)))));
     }
-    std::cout << c << std::endl;
+
+    cout << "Êîðåíü: " << x << endl;
+
+    return 0;
+}
