e9ae386cb08607912dee900d3a8c7d14c85bbfa0
konstantin019 <113022558+konstantin019@users.noreply.github.com>
2022-12-21 16:27:42 +0500

    Create задание 1,1.cpp

---
+++ коды/задание 1.1/задание 1,1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a,b,c,max;
+    cin>>a>>b>>c;
+    max=a;
+    if (b>max){
+        max=b;
+    }
+    if (c>max){
+        max=c;
+    }
+    cout<<max<<endl;
+    return 0;
+}
