f8fbb45940fa91f0afdb9de27158719afb3bb530
konstantin019 <113022558+konstantin019@users.noreply.github.com>
2023-05-22 07:54:44 +0500

    Create обьект.h

---
+++ 2 семестр/классы/лаба 5/обьект.h
@@ -0,0 +1,15 @@
+//
+// Created by 1 on 05.04.2023.
+//
+
+#ifndef MAIN_CPP_OBJECT_H
+#define MAIN_CPP_OBJECT_H
+
+class Object{
+public:
+    Object(void);
+    ~Object();
+    virtual void Show();
+};
+
+#endif //MAIN_CPP_OBJECT
